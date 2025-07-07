8af1b27e907349df7563c825e3905b703e8b94f1
Amith <amith2368@gmail.com>
2025-07-07 22:45:11 +0530

    ts fix

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,5 +1,4 @@
 
-import React from 'react';
 
 export const LoadingSpinner = () => (
   <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-amber-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
